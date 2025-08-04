74d7cd02485d639baa27ff92a41a7da9c87d8052
Mihai Pavelescu <media.expres@gmail.com>
2025-08-04 16:44:59 +0300

    vite config added

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Aici setezi numele subfolderului în care vei publica aplicația.
+  // Asigură-te că pui slash-uri la început și la sfârșit.
+  base: '/proiect-3d/', // <-- IMPORTANT: Schimbă 'proiect-3d' cu numele real al folderului tău!
+})
